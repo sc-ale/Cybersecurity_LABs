ea44522fa6e80d685216eded5cf40e4095b74cf2
AleBombi <118299985+AleBombi@users.noreply.github.com>
2024-04-09 11:10:27 +0200

    aggiunta algoritmo squareNmultiply

--- Lab1/squareNmultiply.c
+++ Lab1/squareNmultiply.c
@@ -3,42 +3,46 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#define IS_BIT_SET(n,x) ((n & (1 << x)))
+
+
+unsigned int binaryLenght(int c) {
+    unsigned int bits;                              
+    unsigned var = (c<0) ? -c : c;              
+    for (bits = 0; var!=0; bits++) var >>= 1;                                    
+    return bits;
+}
+
+
 int squareNmultiply(int x, int c, int n) {
     int z = 1;
-    for (int i = 0; i < c; i++) {
+    
+    for (int i = 0; i < binaryLenght(c)-1; i++) {
         z = (z * z) % n;
-        if ((c & (1 << i)) != 0) {
+        if (IS_BIT_SET(c, i)) {
             z = (z * x) % n;
         }
     }
-    
-    
-    
-
-    if (b0 != 1) {
-        printf("No multiplicative inverse\n");
-        return -1;
-    }
-
-    printf("Multiplicative inverse of %d mod %d is %d\n", a, b, t);
-    return t;
+    printf("%d^%d mod %d = %d\n", x, c, n, z);
+    return z;
 }
 
 
 int main(int argc, const char* argv[]){
-    if (argc != 3) {
+    if (argc != 4) {
         perror("invalid argument numbers\n");
         return EINVAL;
     } 
     
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
+    int x = atoi(argv[1]);
+    int c = atoi(argv[2]);
+    int n = atoi(argv[3]);
 
-    if (a < 0 || b < 0) {
-        perror("arguments must be positive\n");
+    if (n < 0) {
+        perror("argument must be positive\n");
         return EINVAL;
     }
 
-    multliplicativeInverse(a, b);
+    squareNmultiply(x, c, n);
 
 } 
